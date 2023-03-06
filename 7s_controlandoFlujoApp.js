2d300c327688bb9d33f3b5d7e37e2dbb685a54cd
soofypls <sofia.catalina23@gmail.com>
2023-03-06 13:54:20 -0300

    Burguers excercice

--- 7s_controlandoFlujoApp.js
+++ 7s_controlandoFlujoApp.js
@@ -39,66 +39,131 @@ if (esImpar) {
 // }
 // //2. Si tiene 21 años, además de darle la bienvenida, felicitarlo por haber llegado a la mayoría de edad.
 // if (edad = 21) {
-//     console.log("Bienvenido, felicidades por alcanzar la mayoría de edad, ya puedes ingresar y beber alcohol.");
+// //     console.log("Bienvenido, felicidades por alcanzar la mayoría de edad, ya puedes ingresar y beber alcohol.");
+// // }
+// // // 3. Si su edad es impar, decirle en cualquiera de los mensajes: "¿Sabías que tu edad es impar?".
+// // if (edad = edad%1) {
+// //     console.log("¿Sabías que tu edad es impar?");
+// // }
+
+// // EJ #2 Declará una función llamada totalAPagar() que reciba como parámetros: vehiculo y litrosConsumidos.
+
+// // Si el vehículo es “coche”, el precio por litro es de $86.
+// // ● Si es “moto”, ha de ser $70.
+// // ● Si es “autobús”, ha de ser $55.
+// // ● Si los litros consumidos están entre 0 y 25, se ha de añadir $50 al total a pagar.
+// // ● Si los litros consumidos son mayor a 25, se ha de añadir $25 al total a pagar.
+
+
+// //DECLARACION DE VARIABLES
+// let vehiculo;
+// let litrosConsumidos;
+
+// let calculo;
+
+
+
+// do {
+//   vehiculo = prompt("Indique el tipo de vehiculo que posee (moto, coche, autobus): ");
+// } while (vehiculo !== "moto" && vehiculo !== "coche" && vehiculo !=="autobus");
+
+// do {
+//   litrosConsumidos = prompt("Indique la cantidad de litros consumidos por su vehiculo: ")
+//   // litrosConsumidos = toNumber(litrosConsumidos);
+// } while (litrosConsumidos<0);
+
+// let totalAPagar = (vehiculo, litrosConsumidos) => {
+//   let precio;
+//   let adicional;
+
+//   if (litrosConsumidos > 0 && litrosConsumidos <=25){
+//     adicional=50;
+//   } else {
+//     adicional=25;  
+//   }
+
+//   if (vehiculo==="coche"){
+//     precio=86;
+//     // console.log(`El monto a pagar por el coche corresponde a: ${calculo}`);
+//   } else if (vehiculo === "moto"){
+//     precio=70;
+//     // console.log(`El monto a pagar por la moto corresponde a: ${calculo}`);    
+//   } else if (vehiculo==="autobus"){
+//     precio=55;
+//     // console.log(`El monto a pagar por el autobus corresponde a: ${calculo}`);
+//   };
+
+//   return precio + adicional;
 // }
-// // 3. Si su edad es impar, decirle en cualquiera de los mensajes: "¿Sabías que tu edad es impar?".
-// if (edad = edad%1) {
-//     console.log("¿Sabías que tu edad es impar?");
-// }
 
-// EJ #2 Declará una función llamada totalAPagar() que reciba como parámetros: vehiculo y litrosConsumidos.
+// calculo=totalAPagar(vehiculo, litrosConsumidos);
 
-// Si el vehículo es “coche”, el precio por litro es de $86.
-// ● Si es “moto”, ha de ser $70.
-// ● Si es “autobús”, ha de ser $55.
-// ● Si los litros consumidos están entre 0 y 25, se ha de añadir $50 al total a pagar.
-// ● Si los litros consumidos son mayor a 25, se ha de añadir $25 al total a pagar.
+// console.log(`El monto a pagar corresponde a: ${calculo}`)
 
 
-//DECLARACION DE VARIABLES
-let vehiculo;
-let litrosConsumidos;
-
-let calculo;
+// EJ#3   Local de sandwish
 
 
+
+let hamburguesaBase;
+let tipoPan;
+let adicionales;
+let precioFinal;
+let precioHamburguesa;
+let precioPan;
+let precioAdicinal;
 
 do {
-  vehiculo = prompt("Indique el tipo de vehiculo que posee (moto, coche, autobus): ");
-} while (vehiculo !== "moto" && vehiculo !== "coche" && vehiculo !=="autobus");
+  hamburguesaBase = prompt("Indique el tipo de hamburguesa requerida (pollo, carne, veggie): ");
+} while (hamburguesaBase !== "carne" && hamburguesaBase !== "pollo" && hamburguesaBase !=="veggie");
 
 do {
-  litrosConsumidos = prompt("Indique la cantidad de litros consumidos por su vehiculo: ")
-  // litrosConsumidos = toNumber(litrosConsumidos);
-} while (litrosConsumidos<0);
+  tipoPan = prompt("Indique el tipo de pan requerido (blanco, negro, s/gluten): ");
+} while (tipoPan !== "blanco" && tipoPan !== "negro" && tipoPan !=="s/gluten");
 
-let totalAPagar = (vehiculo, litrosConsumidos) => {
-  let precio;
-  let adicional;
+do {
+  adicionales = prompt("Indique el primer adicional que desea agregar (queso, tomate, lechuga, cebolla, mayonesa, mostaza): ");
+} while 
+(adicionales !== "queso" && adicionales !== "tomate" && adicionales !=="lechuga"); && adicionales !== "cebolla" && adicionales !== "mayonesa" && adicionales !== "mostaza"
 
-  if (litrosConsumidos > 0 && litrosConsumidos <=25){
-    adicional=50;
-  } else {
-    adicional=25;  
+let localSandwish = (hamburguesaBase, tipoPan, adicionales) => {
+
+  if (hamburguesaBase==="pollo"){
+    precioHamburguesa=150;
+  } else if (hamburguesaBase==="carne"){
+    precioHamburguesa=200;
+  } else if (hamburguesaBase==="veggie"){
+    precioHamburguesa=100;
   }
 
-  if (vehiculo==="coche"){
-    precio=86;
-    // console.log(`El monto a pagar por el coche corresponde a: ${calculo}`);
-  } else if (vehiculo === "moto"){
-    precio=70;
-    // console.log(`El monto a pagar por la moto corresponde a: ${calculo}`);    
-  } else if (vehiculo==="autobus"){
-    precio=55;
-    // console.log(`El monto a pagar por el autobus corresponde a: ${calculo}`);
-  };
 
-  return precio + adicional;
+  if (tipoPan==="blanco"){
+    precioPan=50;
+  } else if (tipoPan==="negro"){
+    precioPan=60;
+  } else if (tipoPan==="s/gluten"){
+    precioPan=75;
+  }
+
+
+  if (adicionales==="queso"){
+    precioAdicinal=20;
+  } else if (adicionales==="tomate"){
+    precioAdicinal=15;
+  } else if (adicionales==="lechuga"){
+    precioAdicinal=10;
+  } else if (adicionales==="cebolla"){
+    precioAdicinal=15;
+  } else if (adicionales==="mayonesa"){
+    precioAdicinal=5;
+  } else if (adicionales==="mostaza"){
+    precioAdicinal=5;
+  }
+
+  precioFinal = precioHamburguesa + precioPan + precioAdicinal;
+  return precioFinal
+  
 }
 
-calculo=totalAPagar(vehiculo, litrosConsumidos);
-
-console.log(`El monto a pagar corresponde a: ${calculo}`)
-
-
-// EJ#3 
+precioFinal = localSandwish(hamburguesaBase, tipoPan, adicionales);
+console.log(`El monto a pagar corresponde a: ${precioFinal}`);
