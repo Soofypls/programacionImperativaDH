727fd468c168668bf52f9380095feffb023ec207
soofypls <sofia.catalina23@gmail.com>
2023-03-12 02:27:20 -0300

    Adding first try, bank exercice... two undefined..

--- 11a_bank.js
+++ 11a_bank.js
@@ -10,72 +10,73 @@ forma de representar a los usuarios (cuentas bancarias).
 // console.log(num[i]);
  */
 let arrayCuentas = [
-    {
-      nroCuenta: 5486273622,
-      tipoDeCuenta: "Cuenta Corriente",
-      saldoEnPesos: 27771,
-      titularCuenta: "Abigael Natte",
-    },
-    {
-      nroCuenta: 1183971869,
-      tipoDeCuenta: "Caja de Ahorro",
-      saldoEnPesos: 8675,
-      titularCuenta: "Ramon Connell",
-    },
-    {
-      nroCuenta: 9582019689,
-      tipoDeCuenta: "Caja de Ahorro",
-      saldoEnPesos: 27363,
-      titularCuenta: "Jarret Lafuente",
-    },
-    {
-      nroCuenta: 1761924656,
-      tipoDeCuenta: "Cuenta Corriente",
-      saldoEnPesos: 32415,
-      titularCuenta: "Ansel Ardley",
-    },
-    {
-      nroCuenta: 7401971607,
-      tipoDeCuenta: "Cuenta Unica",
-      saldoEnPesos: 18789,
-      titularCuenta: "Jacki Shurmer",
-    },
-  ];
+  {
+    nroCuenta: 5486273622,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 27771,
+    titularCuenta: "Abigael Natte",
+  },
+  {
+    nroCuenta: 1183971869,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 8675,
+    titularCuenta: "Ramon Connell",
+  },
+  {
+    nroCuenta: 9582019689,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 27363,
+    titularCuenta: "Jarret Lafuente",
+  },
+  {
+    nroCuenta: 1761924656,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 32415,
+    titularCuenta: "Ansel Ardley",
+  },
+  {
+    nroCuenta: 7401971607,
+    tipoDeCuenta: "Cuenta Unica",
+    saldoEnPesos: 18789,
+    titularCuenta: "Jacki Shurmer",
+  },
+];
 
-
-
-// let exerciceOneBank = () =>{
-
+let exerciceBankOne = () =>{
 
 let banco = {
-    
-    clientes : arrayCuentas,
+  clientes: arrayCuentas,
 
-    consultarCliente : function(nombreDelTitular){
-        for(let i = 0; i < this.clientes.length; i++){
-            if (this.clientes[i].titularCuenta === nombreDelTitular){
-                return this.clientes[i] }
-        }
-    },
+  consultarCliente: function (nombreDelTitular) {
+    for (let i = 0; i < this.clientes.length; i++) {
+      if (this.clientes[i].titularCuenta === nombreDelTitular) {
+        return this.clientes[i];
+      }
+    }
+  },
 
-    deposito : function(nombre, cantidad){
-        let clienteEncontrado = this.consultarCliente(nombre);
-        clienteEncontrado.saldoEnPesos = clienteEncontrado.saldoEnPesos + cantidad;
-        // clienteEncontrado += cantidad;
-        console.log (`Deposito realizado, su nuevo saldo es de $${clienteEncontrado.saldoEnPesos}`);
-        }
+  deposito: function (nombre, cantidad) {
+    let clienteEncontrado = this.consultarCliente(nombre);
+    clienteEncontrado.saldoEnPesos = clienteEncontrado.saldoEnPesos + cantidad;
+    // clienteEncontrado += cantidad;
+    console.log(
+      `Deposito realizado, su nuevo saldo es de $${clienteEncontrado.saldoEnPesos}`
+    );
+  },
 
-};  
+  extraccion: function (nombre, cantidad) {
+    let clienteEncontrado = this.consultarCliente(nombre);
 
-banco.deposito("Ansel Ardley", 10000);
-console.log(banco.consultarCliente("Ansel Ardley")); 
+    if (clienteEncontrado.saldoEnPesos >= cantidad) {
+      clienteEncontrado.saldoEnPesos -= cantidad;
+      console.log(`Extracción realizada correctamente, su nuevo saldo es de ${clienteEncontrado.saldoEnPesos}`);
+    } else {console.log("Su saldo es insuficiente");
+  }
+}
+}
 
+console.log(banco.deposito("Jarret Lafuente", 100000));
+console.log(banco.extraccion("Jarret Lafuente", 150000));
 
-
-
-
-
-
-    
-// // }
+}
 
