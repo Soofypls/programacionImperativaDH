0cf35a74104399e996e0b01224dc73cf2a3d0dae
soofypls <sofia.catalina23@gmail.com>
2023-03-13 20:06:09 -0300

    Loops exercice, class 13. First step, even and odd

---
+++ 13s_loopDePares.js
@@ -0,0 +1,18 @@
+// let exercide13s_1=()=>{
+
+let loopDePares = (number)=>{
+    //check if the number is even
+    if(number % 2 == 0) {
+        console.log(`El número ${number} es par`);
+    }
+    // if the number is odd
+    else {
+        console.log(`El número ${number} es impar`);
+    }
+
+}
+console.log(loopDePares(8));
+
+// }
+
+// exercide13s_1();
