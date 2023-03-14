d9369d5b4dac71cd371951018fb554b26c3882d8
soofypls <sofia.catalina23@gmail.com>
2023-03-13 21:03:02 -0300

    Adding exercices class 13

--- 13s_loopDePares.js
+++ 13s_loopDePares.js
@@ -1,17 +1,82 @@
 // let exercide13s_1=()=>{
 
-let loopDePares = (number)=>{
-    //check if the number is even
-    if(number % 2 == 0) {
-        console.log(`El número ${number} es par`);
+
+
+/**
+ * Deberás crear una función llamada loopDePares que reciba como parámetro un número 
+ * y haga un loop de 0 a 100 mostrando en la consola cada número del loop. En caso de que el número de la iteración 
+ * sumado con el número pasado por parámetro sea par, mostrará en la consola: “El número X es par”.
+ */
+
+
+
+let loopDePares =(numero)=> {
+    for (let i = 0; i <= 100; i++) {
+      if ((i + numero) % 2 === 0) {
+        console.log(`El número ${numero + i} es par`);
+      } else {
+        console.log(i);
+      }
     }
-    // if the number is odd
-    else {
-        console.log(`El número ${number} es impar`);
-    }
+  }
+// console.log(loopDePares(10));
+
+/**
+ * Deberás crear una función llamada loopDeImpares que reciba como parámetros un número y una palabra, 
+ * y haga un loop de 0 a 100 mostrando en la consola cada número del loop. Luego, modificar el código para que, 
+ * en caso de que ese número sumado con el número pasado por parámetro sea impar, 
+ * muestre en la consola la palabra pasada por parámetro.
+ * 
+ */
+
+let loopDeImpares = (numero, palabra)=>{
+    for (let i = 0; i <= 100; i++) {
+        if ((i + numero) % 2 !== 0) {
+          console.log(`${palabra} `);
+        } else {
+          console.log(i);
+        }
+      }
 
 }
-console.log(loopDePares(8));
+
+// loopDeImpares(7, "Soy un impar");
+
+/**
+ * Deberás crear una función llamada sumatoria que reciba un número como parámetro y que devuelva la sumatoria de todos sus números anteriores, 
+ * incluso ese mismo. Ejemplo:
+sumatoria(3) debe retornar 6 porque hace (1+2+3)
+sumatoria(8) debe retornar 36
+ */
+
+let sumatoria = (numero)=>{
+        let suma = 0;
+        for (let i = 1; i <= numero; i++) {
+          suma += i;
+        }
+        return suma;
+      }
+// sumatoria();
+// console.log(sumatoria(8));
+
+/**
+ * Deberás crear una función llamada nuevoArreglo que reciba un número como parámetro y que devuelva un nuevo arreglo 
+ * con tantos elementos como el número que le hayas pasado. Ejemplo:
+nuevoArreglo(5) debe retornar [1,2,3,4,5]
+nuevoArreglo(10) debe retornar [1,2,3,4,5,6,7,8,9,10]
+ */
+let nuevoArreglo =(numero)=>{
+    let array = [];
+  
+    for (let i = 1; i <= numero; i++) {
+      array.push(i);
+    }
+  
+    return array;
+  }
+
+//   nuevoArreglo();
+//   console.log(nuevoArreglo(5));
 
 // }
 
